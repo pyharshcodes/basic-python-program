dca72c93b730fc0c35bfa2e8be37de03230b820e
pyharshcodes <harshdeepchak@gmail.com>
2025-05-23 13:02:13 +0530

    Add files via upload

---
+++ #Even and odd.py
@@ -0,0 +1,12 @@
+#Even and odd
+#input: A number
+#Output: Chack if the number is even or odd
+
+num = int(input("Enter a number"))
+
+# even - odd check karna
+
+if num%2 == 0:
+    print("The num is even.")
+else:
+    print("The num is odd.")
